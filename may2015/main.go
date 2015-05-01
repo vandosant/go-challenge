161c7d8c7a42d138135c30468905b8a024d32999
vandosant <sjskender@gmail.com>
2015-05-01 15:54:47 -0600

    Generates a histogram of the uploaded image colors

--- may2015/main.go
+++ may2015/main.go
@@ -8,6 +8,8 @@ import (
   "io"
   "encoding/base64"
   "crypto/rand"
+  "image"
+  _ "image/jpeg"
 )
 
 func main() {
@@ -32,12 +34,12 @@ func FileCreateHandler(w http.ResponseWriter, r *http.Request) {
 
   id := random(32)
 
-
   out, err := os.OpenFile("./tmp/testfile"+id, os.O_WRONLY|os.O_CREATE, 0666)
   if err != nil {
     fmt.Println(w, "Unable to create file.")
     return
   }
+
   defer out.Close()
 
   _, err = io.Copy(out, file)
@@ -45,6 +47,37 @@ func FileCreateHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(w, err)
     return
   }
+
+  reader, err := os.Open("./tmp/testfile"+id)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  defer reader.Close()
+
+  m, _, err := image.Decode(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bounds := m.Bounds()
+
+  var histogram [16][4]int
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, a := m.At(x, y).RGBA()
+			// A color's RGBA method returns values in the range [0, 65535].
+			// Shifting by 12 reduces this to the range [0, 15].
+			histogram[r>>12][0]++
+			histogram[g>>12][1]++
+			histogram[b>>12][2]++
+			histogram[a>>12][3]++
+		}
+	}
+
+  fmt.Printf("%-14s %6s %6s %6s %6s\n", "bin", "red", "green", "blue", "alpha")
+	for i, x := range histogram {
+		fmt.Printf("0x%04x-0x%04x: %6d %6d %6d %6d\n", i<<12, (i+1)<<12-1, x[0], x[1], x[2], x[3])
+	}
 
   fmt.Println(w, "File uploaded successfully")
 }
